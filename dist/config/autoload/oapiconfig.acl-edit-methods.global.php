0398d2f3bfb54a27002d85c3c114a2b6b4d5b753
ovimughal <ovi.mughal@gmail.com>
2025-01-07 12:23:04 +0530

    Acl edit method & env changes

---
+++ dist/config/autoload/oapiconfig.acl-edit-methods.global.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'oconfig_manager' => [
+        'aclEditMethods' => [
+            [
+                'controller' => 'association',
+                'action' => 'updateAssociationData'
+            ],
+            [
+                'controller' => 'controllerName2',
+                'action' => 'actionName2'
+            ]
+        ]
+    ]
+];
